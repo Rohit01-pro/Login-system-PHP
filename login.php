c7dd9743f207b1f0c5017324a4ba59153605f86f
Rohit01-pro <rohit777kanojiya@gmail.com>
2020-10-31 18:41:29 +0530

    Initial commit

---
+++ login.php
@@ -0,0 +1,79 @@
+<?php
+require 'connection.php';
+session_start();
+// Logout
+if (isset($_SESSION['user'])) {
+  session_unset();
+  session_destroy();
+  header("Location: index.php");
+}
+
+// Login
+if (isset($_POST['submit'])) {
+  $uname = $_POST['user-name'];
+  $upswd = md5($_POST['user-pswd']);
+  // SQL Query 
+  $query = "Select * From user Where uname='$uname' AND upswd='$upswd'";
+  // echo "Password : " . strcmp("d8578edf8458ce06fbc5bb76a58c5ca4","d8578edf8458ce06fbc5bb76a58c5ca4"); 
+  $result = mysqli_query($connection, $query) or die("Query Failed");
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Login</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+  <?php include 'header.php'; ?>
+  <div class="container">
+    <div class="box">
+      <h1>Login</h1>
+      <?php
+      if (isset($_POST['submit'])) {
+        // echo "Rows: " . mysqli_num_rows($result);
+        if (mysqli_num_rows($result) == 1) {
+          $_SESSION['user'] = $_POST['user-name'];
+          header("Location: index.php");
+        } else
+          echo "<div class='alert danger'>Incorrect Username or Password</div>";
+      }
+      ?>
+      <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
+        <div class="form-group">
+          <div class="input-box">
+            <input type="text" name="user-name" required>
+            <label>User Name</label>
+          </div>
+          <small>
+            <?php if (isset($_POST['submit']) && strlen($_POST['user-name']) < 6)
+              echo "Username must be 6 character."; ?>
+          </small>
+        </div>
+
+        <div class="form-group">
+          <div class="input-box">
+            <input type="password" name="user-pswd" required>
+            <label>Password</label>
+          </div>
+          <small>
+            <?php if (isset($_POST['submit']) && strlen($_POST['user-pswd']) < 6)
+              echo "Password must be 6 character."; ?>
+          </small>
+        </div>
+
+        <input class="btn log-btn" type="submit" name="submit" value="Login">
+        <div class="footer">
+          <p>Not a User? <a href="register.php">Sign Up</a></p>
+        </div>
+      </form>
+    </div>
+  </div>
+
+</body>
+
+</html>
